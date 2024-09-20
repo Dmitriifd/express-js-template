6c56e715cf2ed5941e022e514b39bd6608e78e46
Dmitrii <idimka9@gmail.com>
2024-09-20 22:06:10 +0500

    add eslint-plugin-sonarjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,20 +1,25 @@
 import globals from 'globals';
 import pluginJs from '@eslint/js';
 import tseslint from 'typescript-eslint';
+import sonarjs from 'eslint-plugin-sonarjs';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
   { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: globals.browser } },
+  sonarjs.configs.recommended,
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
   {
     rules: {
-      '@typescript-eslint/no-unused-vars': ['error', { 
+      '@typescript-eslint/no-unused-vars': ['error', {
         argsIgnorePattern: '^_',
         varsIgnorePattern: '^_',
         caughtErrorsIgnorePattern: '^_'
       }],
+      'sonarjs/new-cap': 'warn',
+      'sonarjs/cors': 'warn',
+      "sonarjs/cognitive-complexity": ["warn", 3],
     }
   },
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
 ];
